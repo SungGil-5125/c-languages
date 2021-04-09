7c5d306e4adb705a6551d2046327c70fe63fd538
성길 <sunggil0125@naver.com>
2021-04-09 21:51:15 +0900

    c_언어

--- hello world/hello world/hello world.c
+++ hello world/hello world/hello world.c
@@ -25,7 +25,10 @@ int main()
 		else
 			printf("f학점 입니다");
 	}
+
+
 */
+
 	/*int x, answer;
 	printf("숫자 맞추기 게임\n\n");
 
@@ -40,7 +43,9 @@ int main()
 	}
 	else {
 		printf("오답입니다 정답은 %d입니다\n", answer);
-	}*/
+	}
+
+
 
 	/*char ch;
 
@@ -58,13 +63,16 @@ int main()
 		printf("%c는 숫자입니다", ch);
 
 	else if (ch >= 33 && ch <= 47)
-		printf("%c는 가타문자 입니다", ch);
+		printf("%c는 가타문자 입니다", ch);*/
+
+
+
 
 	/*int num1, num2;
 	char ch;
 	printf("수식을 입력하세요(예: 2 + 5):");
 	scanf_s("%d", &num1);
-	scanf_s("%c", &ch);
+	scanf_s("%c", &ch, sizeof(ch));
 	scanf_s("%d", &num2);
 
 	if (ch == '+')
@@ -78,10 +86,10 @@ int main()
 	else if (ch == '%')
 		printf("%d % %d = %d", num1, num2, num1 % num2);
 	else
-		printf("지원되지 않는 연산자 입니다");
-		*/
+		printf("지원되지 않는 연산자 입니다");*/
 	
-	
+
+
 
 	/*char a, b;
 	printf("문자를 입력하세요");
@@ -110,7 +118,9 @@ int main()
 	
 	*/
 
-	int user, answer, win=0, lose=0, same=0, pan=0;
+
+
+	/*int user, answer, win=0, lose=0, same=0, pan=0;
 	
 	while (1)
 	{
@@ -143,8 +153,184 @@ int main()
 	}
 
 	printf("게임을 종료합니다. %d판 %d승 %d패 %d무 하셨습니다.", pan, win, lose, same);
+	*/
 	
+	/*int num1;
 
+	printf("정수 입력:");
+	scanf_s("%d", &num1);
+
+	switch (num1)
+	{
+	case 0:
+		printf("%d는 3의 배수가 아닙니다", num1);
+		break;
+	case 1:
+		printf("%d는 3의 배수가 아닙니다", num1);
+		break;
+	case 2:
+		printf("%d는 3의 배수가 아닙니다", num1);
+		break;
+	case 3: 
+		printf("%d는 3의 배수입니다", num1);
+		break;
+	
+	
+	}*/
+
+	/*int moon;
+
+	printf("달 입력:");
+	scanf_s("%d", &moon);
+
+	switch (moon) {
+		
+	case 2: printf("%d월의 일수는 28일 입니다", moon);
+		break;
+	case 4:
+	case 6:
+	case 8:
+	case 10:
+	case 12:printf("%d월의 일수는 30일 입니다", moon);
+		break;
+
+	case 1:
+	case 3:
+	case 5:
+	case 7:
+	case 9:
+	case 11:
+		printf("%d월의 일수는 30일 입니다", moon);
+		break;
+	}
+	*/
+
+	/*int  num1, num2;
+	char ch;
+	printf("수식을 입력하세요:");
+	scanf_s("%d", &num1);
+	scanf_s("%c", &ch,sizeof(ch));
+	scanf_s("%d", &num2);
+
+	switch(ch){
+	case '*':
+		printf("%d * %d = %d", num1, num2, num1 * num2);
+		break;
+	case '+':
+		printf("%d * %d = %d", num1, num2, num1 + num2);
+		break;
+	case '-':
+		printf("%d * %d = %d", num1, num2, num1 - num2);
+		break;
+	case '/':
+		printf("%d * %d = %d", num1, num2, num1 / num2);
+		break;
+	case '%':
+		printf("%d * %d = %d", num1, num2, num1 % num2);
+		break;
+	default:
+		printf("지원되지 않는 연산자 입니다.");
+		break;
+	}*/
+
+	/*int score;
+	
+	printf("성적을 입력하세요:");
+	scanf_s("%d", &score);
+	
+		if (score <= 0&&score>=100)
+		{
+			switch (score / 10)
+			{
+
+			case 9:
+				printf("A학점");
+				break;
+			case 8:
+				printf("B학점");
+				break;
+			case 7:
+				printf("C학점");
+				break;
+			case 6:
+				printf("D학점");
+				break;
+			default:
+				printf("f학점");
+				break;
+			}
+		} 
+		else
+			printf("점수 허용 구간이 아닙니다.다시 시작하세요");*/
+
+
+	/*int car,num1,num2;
+	char chose;
+
+	printf("자동차 모델을 선택하시겠습니까?(Y/N):");
+	scanf_s("%c", &chose,sizwof(car));
+	
+	switch(chose) 
+	{
+		case 'y':
+			printf("자동차 예약 현왕: 0  0  0\n");
+			printf("자동차 모델 선택(1 ~ 3번 중 선택):");
+			scanf_s("%d", &car);
+			break;
+		case 'n':
+			printf("자동차 예약 프로그램을 종료합니다");
+			break;
+		case 'Y':
+			printf("자동차 예약 현왕: 0  0  0\n");
+			printf("자동차 모델 선택(1 ~ 3번 중 선택):");
+			scanf_s("%d", &car);
+			break;
+		case 'N':
+			printf("자동차 예약 프로그램을 종료합니다\n");
+			break;
+		default:
+			printf("알파벳 입력오류\n");
+			break;
+	}
+
+	if (car == 1) {
+		printf("예약 완료되었습니다.\n");
+		printf("자동차 예약 현황 : 1  0  0\n");
+	}
+	if (car == 2) {
+		printf("예약 완료되었습니다.\n");
+		printf("자동차 예약 현황 : 0  1  0\n");
+	}if (car == 3) {
+		printf("예약 완료되었습니다.\n");
+		printf("자동차 예약 현황 : 0  0  1\n");
+	}
+	*/	
+	
+	int num,num2,num3;
+	int i, n, p;
+	char chose;
+
+	printf("자동차 모델을 선택하시겠습니까?");
+	scanf_s("%c", &chose);
+
+	switch (chose) {
+
+	case 'y':
+		printf("자동차 모델 번호: 1  2  3\n");
+		printf("자동차 예약 현황: %d %d  %d\n",i,n,p);
+	case 'Y':
+		printf("자동차 모델 번호: 1  2  3\n");
+		printf("자동차 예약 현황: %d %d  %d\n", i, n, p);
+	case 'N':
+		printf("자동차 모델 No.[%d][%d] 예약 완료 후 프로그램 종료");
+	case 'n':
+		printf("자동차 모델 No.[%d][%d] 예약 완료 후 프로그램 종료");
+	default:
+		printf("알파벳 입력 오류입니다\n");
+		printf("알파벳은 대소문자 구별\n");
+		printf("알파벳 입력 오류입니다\n");
+
+	}
 
 
 	return 0;
